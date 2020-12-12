3d45cdaa8439488dc84f10cf8dcf3056c54ad204
Michael White <crazytacolover47@gmail.com>
2020-12-11 23:18:12 -0500

    Update canvas.java

--- CatHacks/src/canvas.java
+++ CatHacks/src/canvas.java
@@ -1,77 +1,138 @@
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class MyImageObj extends JLabel {
-    private BufferedImage bim = null;
+public class canvas extends JFrame {
+    private Container c = getContentPane();
+    private JPanel controls = new JPanel();
+    private JPanel imageBoard = new JPanel();
+    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private Color bg = new Color(178, 213,224);
+    private BufferedImage image;
+    private MyImageObj pbnImage = new MyImageObj(), usrImg = new MyImageObj();
 
-    private Color[] colors = {
-            Color.black,
-            Color.white,
-            Color.gray,
-            Color.lightGray,
-            Color.RED,
-            Color.ORANGE,
-            Color.YELLOW,
-            Color.GREEN,
-            Color.BLUE,
-            Color.cyan,
-            Color.DARK_GRAY,
-            Color.magenta,
-            Color.pink
-    };
+    public canvas(){
+        super("Paint by number");
+        buildMenus();
 
-    MyImageObj(){
+        setSize(screenSize);
+        c.setBackground(bg);
+        setVisible(true);
+        c.add(controls, BorderLayout.SOUTH);
+        c.add(imageBoard, BorderLayout.CENTER);
+        setControls();
+        imageDisplay();
+    }
+    public void imageDisplay(){
+        GridLayout gl = new GridLayout(1,2);
+        gl.setHgap(200);
+        imageBoard.setBackground(bg);
+        imageBoard.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight() - 50);
+        imageBoard.setBorder(BorderFactory.createEmptyBorder(15,50,15,50));
+        imageBoard.setLayout(gl);
 
+        // add images to panel here
+        imageBoard.add(usrImg);
+        imageBoard.add(pbnImage);
+    }
+    public void setControls(){
+        GridLayout gl = new GridLayout(2, 1);
+        gl.setVgap(15);
+        controls.setLayout(gl);
+        controls.setBorder(BorderFactory.createEmptyBorder(15,50,15,50));
+        controls.setSize((int) screenSize.getWidth(), 50);
+        controls.setBackground(bg);
+        JSlider numValues = new JSlider();
+        numValues.setMinimum(2);
+        numValues.setMaximum(15);
+        numValues.setValue(8);
+        numValues.setPaintLabels(true);
+        numValues.setPaintTicks(true);
+        numValues.setMajorTickSpacing(1);
+        numValues.setBackground(bg);
+        controls.add(numValues);
+        JButton start = new JButton("Start");
+        JPanel buttons = new JPanel();
+        buttons.setBackground(bg);
+        buttons.add(start);
+        controls.add(buttons);
+
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pbnImage.thresholdImage(3);
+            }
+        });
     }
 
-    public void setImage(BufferedImage img){
-        if(img == null){
-            return;
-        }
-        bim = img;
+    public void buildMenus(){
+        final JFileChooser fc = new JFileChooser(".");
+        JMenuBar bar = new JMenuBar();
+        this.setJMenuBar (bar);
+        JMenu fileMenu = new JMenu ("File");
+        JMenuItem sourceOpen = new JMenuItem ("Source Image");
+        JMenuItem fileexit = new JMenuItem ("Exit");
 
-        this.repaint();
-    }
+        sourceOpen.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed (ActionEvent e) {
+                        int returnVal = fc.showOpenDialog(canvas.this);
+                        if (returnVal == JFileChooser.APPROVE_OPTION) {
+                            File file = fc.getSelectedFile();
+                            try {
+                                image = ImageIO.read(file);
+                            } catch (IOException e1){}
 
-    public BufferedImage getImage(){
-        return bim;
-    }
+                            usrImg.setImage(image);
+                            pbnImage.setImage(image);
 
-    public void thresholdImage(int i){
+                            // resize the image
+                            if(usrImg.getImage().getWidth() < (screenSize.getWidth()/2) - 100){
+                                double ratio = (screenSize.getWidth()/2 - 100 )/ usrImg.getImage().getWidth();
+                                //double ratio = usrImg.getImage().getWidth()/usrImg.getImage().getHeight();
+                                BufferedImage resizedImage = new BufferedImage((int)screenSize.getWidth()/2 - 100, (int)(usrImg.getImage().getHeight()*ratio), usrImg.getImage().getType());
+                                Graphics2D g2d = (Graphics2D) resizedImage.getGraphics();
+                                g2d.drawImage(image,0,0,(int)screenSize.getWidth()/2 - 100,(int)(usrImg.getImage().getHeight()*ratio),null);
+                                g2d.dispose();
+                                usrImg.setImage(resizedImage);
 
-        int colorIndex = 0;
-        for(int y = 0; y < bim.getHeight(); y++){
-            for(int x = 0; x < bim.getWidth(); x++){
-                double min = 99999;
-                int pixel = bim.getRGB(x,y);
-                Color clr = new Color(pixel,true);
-                int red = clr.getRed(), blue = clr.getBlue(), green = clr.getGreen();
-                for( int n = 0; n < colors.length;n++){
-                    int j = (colors[n].getRed() - red), k = (colors[n].getBlue() - blue), m = (colors[n].getGreen() - green);
-                    double distance = Math.sqrt((j*j)+(k*k)+(m*m));
-                    System.out.println("distance: " + distance);
-                    if(distance < min){
-                        min = distance;
-                        colorIndex = n;
-                        //System.out.println("Min: " + min);
+                                resizedImage = new BufferedImage(usrImg.getImage().getWidth(), usrImg.getImage().getHeight(), usrImg.getImage().getType());
+                                g2d = (Graphics2D) resizedImage.getGraphics();
+                                g2d.drawImage(image,0,0,usrImg.getImage().getWidth(),usrImg.getImage().getHeight(),null);
+                                g2d.dispose();
+
+                                pbnImage.setImage(resizedImage);
+                            }
+
+                            usrImg.repaint();
+                            pbnImage.repaint();
+                        }
                     }
                 }
-                System.out.println("index: " + colorIndex);
-                System.out.println("------------------");
-                bim.setRGB(x,y,colors[colorIndex].getRGB());
+        );
+        fileexit.addActionListener(
+                new ActionListener () {
+                    public void actionPerformed (ActionEvent e) {
+                        System.exit(0);
+                    }
+                }
+        );
 
-            }
-        }
-        repaint();
+        fileMenu.add(sourceOpen);
+        fileMenu.add(fileexit);
+        bar.add(fileMenu);
     }
-
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D big = (Graphics2D) g;
-        g.clearRect(0,0,getWidth(),getHeight());
-
-        big.drawImage(bim,0,0,this);
+    public static void main(String[] args){
+        canvas c = new canvas();
+        c.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) { System.exit(0); }
+        });
     }
 }
