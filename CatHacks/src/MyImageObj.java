9330600c96ff0dc3cbe1d3d6509c3982b1d18618
Michael White <crazytacolover47@gmail.com>
2020-12-11 23:19:42 -0500

    Update MyImageObj.java

--- CatHacks/src/MyImageObj.java
+++ CatHacks/src/MyImageObj.java
@@ -1,11 +1,27 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class MyImageObj extends JLabel {
     private BufferedImage bim = null;
-    private boolean isPBN = false;
-    private int threshVal = 8;
+
+    private Color[] colors = {
+            Color.black,
+            Color.white,
+            Color.gray,
+            Color.lightGray,
+            Color.RED,
+            Color.ORANGE,
+            Color.YELLOW,
+            Color.GREEN,
+            Color.BLUE,
+            Color.cyan,
+            Color.DARK_GRAY,
+            Color.magenta,
+            Color.pink
+    };
+
     MyImageObj(){
 
     }
@@ -23,8 +39,33 @@ public class MyImageObj extends JLabel {
         return bim;
     }
 
-    public void setThreshVal(int val) { threshVal = val; }
-    public void setPBN() { isPBN = true; }
+    public void thresholdImage(int i){
+
+        int colorIndex = 0;
+        for(int y = 0; y < bim.getHeight(); y++){
+            for(int x = 0; x < bim.getWidth(); x++){
+                double min = 99999;
+                int pixel = bim.getRGB(x,y);
+                Color clr = new Color(pixel,true);
+                int red = clr.getRed(), blue = clr.getBlue(), green = clr.getGreen();
+                for( int n = 0; n < colors.length;n++){
+                    int j = (colors[n].getRed() - red), k = (colors[n].getBlue() - blue), m = (colors[n].getGreen() - green);
+                    double distance = Math.sqrt((j*j)+(k*k)+(m*m));
+                    System.out.println("distance: " + distance);
+                    if(distance < min){
+                        min = distance;
+                        colorIndex = n;
+                        //System.out.println("Min: " + min);
+                    }
+                }
+                //System.out.println("index: " + colorIndex);
+                //System.out.println("------------------");
+                bim.setRGB(x,y,colors[colorIndex].getRGB());
+
+            }
+        }
+        repaint();
+    }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
