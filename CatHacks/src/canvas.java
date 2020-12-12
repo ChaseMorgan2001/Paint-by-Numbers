cb6d87b221ea2c93dc09f1ee909eaa858acd52a7
Chase Ballard <44304471+ChaseMorgan2001@users.noreply.github.com>
2020-12-11 19:45:36 -0500

    Update canvas.java

--- CatHacks/src/canvas.java
+++ CatHacks/src/canvas.java
@@ -1,5 +1,15 @@
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class canvas {
-    
+public class canvas extends JFrame {
+    public canvas(){
+
+    }
+    public static void main(String[] args){
+        canvas c = new canvas();
+        c.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) { System.exit(0); }
+        });
+    }
 }
