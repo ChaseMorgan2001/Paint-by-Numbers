7ccd8b1c21a86948ebfeabd97c7c9c9e795406ae
Chase Ballard <44304471+ChaseMorgan2001@users.noreply.github.com>
2020-12-12 11:41:10 -0500

    Update MyImageObj.java

--- CatHacks/src/MyImageObj.java
+++ CatHacks/src/MyImageObj.java
@@ -5,17 +5,14 @@ import java.awt.image.BufferedImageOp;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MyImageObj extends JLabel {
     private BufferedImage bim = null, og = null;
 
     private ArrayList<Color> colors;
 
-    //private ArrayList<Color> colors = new ArrayList<Color>();
-
-    MyImageObj(){
-
-    }
+    MyImageObj(){ }
 
     public void setImage(BufferedImage img){
         if(img == null){
@@ -35,26 +32,6 @@ public class MyImageObj extends JLabel {
         return bim;
     }
 
-    public void findColorSpace(){
-
-        for(int y = 0; y < bim.getHeight(); y++){
-            for(int x = 0; x < bim.getWidth(); x++){
-                boolean insert = true;
-                int pixel = bim.getRGB(x,y);
-                Color clr = new Color(pixel,true);
-                int red = clr.getRed(), blue = clr.getBlue(), green = clr.getGreen();
-                for( int n = 0; n < colors.size();n++){
-                    if(colors.get(n) == clr){
-                        insert = false;
-                    }
-                }
-                if(insert){
-                    //colors.add(clr);
-                }
-            }
-        }
-    }
-
     public void blurImage(){
         float[] matrix = {
                 0.111f, 0.111f, 0.111f,
@@ -68,6 +45,20 @@ public class MyImageObj extends JLabel {
                 0.024879f,	0.107973f,	0.024879f
         };
         BufferedImageOp op = new ConvolveOp(new Kernel(3,3,gauss));
+        BufferedImage temp = new BufferedImage(bim.getWidth(), bim.getHeight(), bim.getType());
+        op.filter(bim,temp);
+        bim = temp;
+
+        repaint();
+    }
+
+    public void sharpen(){
+        float[] edge = {
+                0, -1, 0,
+                -1, 5, -1,
+                0, -1, 0
+        };
+        BufferedImageOp op = new ConvolveOp(new Kernel(3,3,edge));
         BufferedImage temp = new BufferedImage(bim.getWidth(), bim.getHeight(), bim.getType());
         op.filter(bim,temp);
         bim = temp;
@@ -143,7 +134,18 @@ public class MyImageObj extends JLabel {
         //System.out.println("Done");
         repaint();
     }
-
+    public void defineLines(){
+        for (int x = 0; x < bim.getWidth(); x++) {
+            for (int y = 0; y < bim.getHeight(); y++) {
+                int rgba = bim.getRGB(x, y);
+                Color col = new Color(rgba, true);
+                if (col.getRGB() != Color.WHITE.getRGB()){
+                    col = Color.black;
+                }
+                bim.setRGB(x, y, col.getRGB());
+            }
+        }
+    }
     public void invertImage() {
         for (int x = 0; x < bim.getWidth(); x++) {
             for (int y = 0; y < bim.getHeight(); y++) {
@@ -152,16 +154,19 @@ public class MyImageObj extends JLabel {
                 col = new Color(255 - col.getRed(),
                         255 - col.getGreen(),
                         255 - col.getBlue());
-                if (col.getRGB() != Color.WHITE.getRGB()){
-                    col = Color.black;
-                }
+
                 bim.setRGB(x, y, col.getRGB());
             }
         }
     }
     public void setColors(int i) {
         colors = getDominantColors(i);
+        System.out.println(Color.YELLOW.getRed() + ", " + Color.YELLOW.getGreen() + ", " + Color.YELLOW.getBlue());
+        for (int n = 0; n < colors.size(); n++){
+            System.out.println(colors.get(n).getRed() + ", " + colors.get(n).getGreen() + ", " + colors.get(n).getBlue());
+        }
     }
+
     public ArrayList<Color> getDominantColors(int i){
         ArrayList<Color> colors = new ArrayList<>();
         ArrayList<Color> mainColors = new ArrayList<>();
@@ -188,10 +193,47 @@ public class MyImageObj extends JLabel {
             if (mainColors.size() < i) {
                 mainColors.add(colors.get(howMuch.indexOf(max)));
                 colors.remove(howMuch.indexOf(max));
-                howMuch.remove(max);
+                howMuch.remove((Integer) max);
             }
         }
         return mainColors;
+    }
+
+    public void randomThreshHold(int i){
+        Color[] randomPallete = new Color[i];
+        randomPallete[0] = Color.black;
+        randomPallete[1] = Color.white;
+        if(i > 2){
+            for(int j = 2; j < i;j++ ){
+                Random random = new Random();
+                int x = random.nextInt(bim.getWidth());
+                int y = random.nextInt(bim.getHeight());
+                int pixel = bim.getRGB(x,y);
+                Color clr = new Color(pixel,true);
+                randomPallete[j] = clr;
+            }
+        }
+        int colorIndex = 0;
+        for(int y = 0; y < bim.getHeight(); y++){
+            for(int x = 0; x < bim.getWidth(); x++){
+                double min = 99999;
+                int pixel = bim.getRGB(x,y);
+                Color clr = new Color(pixel,true);
+                int red = clr.getRed(), blue = clr.getBlue(), green = clr.getGreen();
+                for( int n = 0; n < Math.min(i, colors.size());n++){
+                    int j = (randomPallete[n].getRed() - red), k = (randomPallete[n].getBlue() - blue), m = (randomPallete[n].getGreen() - green);
+                    double distance = Math.sqrt((j*j)+(k*k)+(m*m));
+                    if(distance < min){
+                        min = distance;
+                        colorIndex = n;
+                    }
+                }
+                bim.setRGB(x,y,randomPallete[colorIndex].getRGB());
+                //repaint();
+            }
+        }
+        System.out.println("Done");
+        repaint();
     }
 
     public void grayscaleImage(){
@@ -216,17 +258,5 @@ public class MyImageObj extends JLabel {
         g.clearRect(0,0,getWidth(),getHeight());
 
         big.drawImage(bim,0,0,this);
-    }
-}
-
-class colorFreq {
-    Color c;
-    int count = 0;
-    colorFreq(Color clr){
-        c = clr;
-        count++;
-    }
-    public void incrCount(){
-        count++;
     }
 }
