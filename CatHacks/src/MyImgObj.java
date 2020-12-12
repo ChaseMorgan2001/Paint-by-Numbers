791a208d6e56639626697bf4555979b99d8c8375
Chase Ballard <44304471+ChaseMorgan2001@users.noreply.github.com>
2020-12-11 21:15:53 -0500

    Create MyImgObj.java

---
+++ CatHacks/src/MyImgObj.java
@@ -0,0 +1,33 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MyImageObj extends JLabel {
+    private BufferedImage bim = null;
+
+    MyImageObj(){
+
+    }
+
+    public void setImage(BufferedImage img){
+        if(img == null){
+            return;
+        }
+        bim = img;
+
+        this.repaint();
+    }
+
+    public BufferedImage getImage(){
+        return bim;
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D big = (Graphics2D) g;
+        g.clearRect(0,0,getWidth(),getHeight());
+
+        big.drawImage(bim,0,0,this);
+    }
+}
